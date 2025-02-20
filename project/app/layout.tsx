59d5af386df3d5d56b9e1c7150b86af3d857ea09
Tolga BAYRAK <bayraktolga28@gmail.com>
2025-02-20 21:23:34 +0300

    analytics

--- project/app/layout.tsx
+++ project/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react"
 import { Inter } from "next/font/google";
 import "./globals.css";
 
@@ -85,6 +86,7 @@ export default function RootLayout({
       </head>
       <body className={`${inter.className} antialiased`}>
         {children}
+        <Analytics />
       </body>
     </html>
   );
