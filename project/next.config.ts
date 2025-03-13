e255ef523b05808a0f4be317a5aa4a78acaa18be
Tolga BAYRAK <bayraktolga28@gmail.com>
2025-03-13 23:40:14 +0300

    file

--- project/next.config.ts
+++ project/next.config.ts
@@ -1,7 +1,55 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'www.giresunhakkinda.com',
+          },
+        ],
+        permanent: true,
+        destination: 'https://giresunhakkinda.com/:path*',
+      },
+    ]
+  },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'X-Robots-Tag',
+            value: 'index, follow'
+          },
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=31536000; includeSubDomains'
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff'
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY'
+          }
+        ],
+      },
+    ]
+  },
+  images: {
+    domains: ['giresunhakkinda.com'],
+    minimumCacheTTL: 60,
+  },
+  trailingSlash: false,
+  poweredByHeader: false,
+  env: {
+    NEXT_PUBLIC_SITE_URL: 'https://giresunhakkinda.com',
+  }
 };
 
 export default nextConfig;
